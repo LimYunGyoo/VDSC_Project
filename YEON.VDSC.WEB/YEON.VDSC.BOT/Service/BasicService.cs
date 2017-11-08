dde14bad4f7b16dd03cb4898fd999362f021a8ca
lim_yungyoo <lim_yungyoo@elandsystems.com>
2017-11-08 17:10:06 +0900

    add services and dao

---
+++ YEON.VDSC.WEB/YEON.VDSC.BOT/Service/BasicService.cs
@@ -0,0 +1,31 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using YEON.VDSC.CORE.Domain;
+
+namespace YEON.VDSC.BOT.Service
+{
+    public abstract class BasicService
+    {
+        protected const int discountMinimum = 50;
+        public abstract void getDiscountProducts();
+        protected static HtmlNodeCollection ParseHTML(string strUri, string nodeDiv)
+        {
+            HttpClient http = new HttpClient();
+            Encoding utf = Encoding.GetEncoding("utf-8");
+            HtmlDocument document = new HtmlDocument();
+
+            Stream stream_source = http.GetStreamAsync(strUri).Result;
+            StreamReader reader = new StreamReader(stream_source, utf);
+            string html = reader.ReadToEnd();
+            document.LoadHtml(html);
+            HtmlNodeCollection results = document.DocumentNode.SelectNodes(nodeDiv);
+
+            return results;
+
+        }
+    }
+}
---
+++ YEON.VDSC.WEB/YEON.VDSC.CORE/Dao/GmarketDao.cs
@@ -0,0 +1,47 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using YEON.VDSC.CORE.Domain;
+
+namespace YEON.VDSC.CORE.Dao
+{
+    public interface IGmarketDao
+    {
+        void InsertProducts(IList<Product> products);
+        IList<Product> SelectOverProducts(int discount);
+    }
+
+    public class GmarketDao : IGmarketDao
+    {
+        MongoClient cli;
+        IMongoDatabase db;
+        IMongoCollection<Product> collection;
+        private static readonly string connString = "mongodb://localhost";
+        private static readonly string databaseString = "VDSCDB";
+        private static readonly string collectionsString = "gmarket";
+
+        public GmarketDao()
+        {
+            cli = new MongoClient(connString);
+            db = cli.GetDatabase(databaseString);
+            collection = db.GetCollection<Product>(collectionsString);
+        }
+
+        public void InsertProducts(IList<Product> products)
+        {
+            db.DropCollection(collectionsString);
+            collection.InsertMany(products);
+        }
+
+        public IList<Product> SelectOverProducts(int discount)
+        {
+            var filter = Builders<Product>.Filter.AnyGte("discount", discount);
+            IList<Product> results = collection.Find(filter).SortByDescending(x => x.Discount).ToList<Product>();
+
+            return results;
+        }
+
+    }
+}
